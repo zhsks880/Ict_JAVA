fdd7f9a43cba523d3dd75fef36b8e957ed3f7547
zhsks880 <110085497+zhsks880@users.noreply.github.com>
2022-08-26 10:01:32 +0900

    first commit

---
+++ 2022_0823_1일차/Hello.java
@@ -0,0 +1,11 @@
+public class Hello
+{
+	public static void main(String [] args)
+	{
+		int weight = 30;
+		int age = 30;
+		System.out.println("Hi!~ Everyone!!");
+		System.out.println("나는 홍길동인데 몸무게가 : " + weight + "kg");
+		System.out.println("나는 홍길동인데 나이가 : " + age + "살");
+	}
+}
---
+++ 2022_0823_1일차/MyProfile.java
@@ -0,0 +1,10 @@
+public class MyProfile
+{
+	public static void main(String [] args)
+	{
+		System.out.println("이름 : 전상현");
+		System.out.println("나이 : 20살");
+		System.out.println("취미 : 밥먹기");
+		System.out.println("주소 : 경기도 고양시");
+	}
+}
